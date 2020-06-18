4ac0c20fff68b9c96bfb90324687d16d4b0d8d69
gdelataillade <gautier2406@gmail.com>
2020-06-18 17:29:38 +0200

    ContactList: Add basic layout

--- lib/views/contact_list.dart
+++ lib/views/contact_list.dart
@@ -11,7 +11,55 @@ class _ContactListState extends State<ContactList> {
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width * 0.35,
-      decoration: BoxDecoration(),
+      color: Colors.blue,
+      child: Container(
+        height: 50,
+        width: 50,
+        margin: const EdgeInsets.only(top: 10, left: 15, right: 15),
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _SearchBar(),
+            _ItemsList(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SearchBar extends StatefulWidget {
+  @override
+  __SearchBarState createState() => __SearchBarState();
+}
+
+class __SearchBarState extends State<_SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      color: Colors.grey,
+      height: 50,
+      width: MediaQuery.of(context).size.width * 0.3,
+    );
+  }
+}
+
+class _ItemsList extends StatefulWidget {
+  @override
+  __ItemsListState createState() => __ItemsListState();
+}
+
+class __ItemsListState extends State<_ItemsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.brown,
+      ),
     );
   }
 }
