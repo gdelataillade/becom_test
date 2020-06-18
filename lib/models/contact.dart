aba49c9c7cf2c6eb0b6198f48a9e8a5d76f999b3
gdelataillade <gautier2406@gmail.com>
2020-06-18 18:02:11 +0200

    Contact: Init contacts list with fake data

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -1,16 +1,40 @@
 import 'package:scoped_model/scoped_model.dart';
+import 'package:faker/faker.dart';
 
 class Contact {
-  String _name;
-  String _phone;
-  String _email;
-  String _address;
-  String _zip;
-  String _city;
-  String _birthDate;
-  String _comment;
+  String name;
+  String phone;
+  String email;
+  String address;
+  String zip;
+  String city;
+  String birthDate;
+  String comment;
 }
 
 class ContactModel extends Model {
   List<Contact> _contactList = [];
+
+  void _initContacts() {
+    Faker faker = Faker();
+    int contactsNb = 50;
+
+    for (var i = 0; i < contactsNb; i++) {
+      Contact newContact = Contact();
+
+      newContact.name = faker.person.name();
+      newContact.phone = "06 65 65 65 65";
+      newContact.email = faker.internet.email();
+      newContact.address = faker.address.streetAddress();
+      newContact.zip = faker.address.zipCode();
+      newContact.city = faker.address.city();
+      newContact.birthDate = faker.date.time();
+      newContact.comment = faker.lorem.sentence();
+      _contactList.add(newContact);
+    }
+  }
+
+  ContactModel() {
+    _initContacts();
+  }
 }
