0ba028d42b53384f704d81f51ce3272d0dd384c8
gdelataillade <gautier2406@gmail.com>
2020-06-18 20:05:49 +0200

    ContactInfo: Add basic layout for header

--- lib/views/contact_info.dart
+++ lib/views/contact_info.dart
@@ -17,13 +17,66 @@ class _ContactInfoState extends State<ContactInfo> {
       color: Colors.red,
       child: ScopedModelDescendant<ContactModel>(
         builder: (context, child, model) {
-          return Center(
-            child: Text(
-              model.contactList[model.selectedContactIndex].name,
-            ),
+          return Column(
+            children: <Widget>[
+              _Header(),
+              _ContactType(),
+              _Form(),
+            ],
           );
         },
       ),
     );
+  }
+}
+
+class _Header extends StatefulWidget {
+  @override
+  __HeaderState createState() => __HeaderState();
+}
+
+class __HeaderState extends State<_Header> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: MediaQuery.of(context).size.width * 0.65,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 80, top: 28),
+        child: Text(
+          "Infos",
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactType extends StatefulWidget {
+  @override
+  __ContactTypeState createState() => __ContactTypeState();
+}
+
+class __ContactTypeState extends State<_ContactType> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
